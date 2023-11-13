5de52c7720e73671330ceca1737f9fe70132bc68
Michal Miler <michal.niemodlin@gmail.com>
2023-11-13 14:29:01 +0100

    added searching albums

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,35 @@
 
 use base64::{Engine as _, engine::general_purpose};
+use reqwest::Url;
+use reqwest::header::{HeaderValue, HeaderMap};
+use serde_json::Value;
 use std::error::Error;
+use std::fs::File;
+use urlencoding;
 
-pub fn encode_base64(client_id: &String, client_secret: &String) -> String {
+fn encode_base64(client_id: &String, client_secret: &String) -> String {
     let string_to_encode = format!("{}:{}", client_id, client_secret);
-
     general_purpose::STANDARD.encode(string_to_encode)
 }
 
-pub async fn get_access_token(encoded_string: &String) -> Result<(), Box<dyn Error>> {
-    let encoded_arg = format!("Basic {}", encoded_string);
+pub async fn get_access_token(client: &reqwest::Client, client_id: &String, client_secret: &String) -> Result<String, Box<dyn Error>> {
+    let encoded_string = encode_base64(&client_id, &client_secret);
 
-    println!("{}", encoded_arg);
+    let params = [("grant_type", "client_credentials")];
 
-    let client = reqwest::Client::new()
+    let result = client
         .post("https://auth.tidal.com/v1/oauth2/token")
-        .header("Authorization", encoded_arg)
-        .body("grant_type=client_credentials")
+        .header("Authorization", &format!("Basic {}", encoded_string))
+        .form(&params)
         .send()
+        .await?
+        .text()
         .await?;
 
-    println!("{:#?}", client);
+    let data: Value = serde_json::from_str(&result)?;
+    let access_token = String::from(data["access_token"].as_str().unwrap());
 
-    Ok(())
+    Ok(access_token)
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,40 @@
-use reqwest::header::{self, HeaderName, HeaderValue, HeaderMap};
+use reqwest::Url;
+use reqwest::header::{HeaderValue, HeaderMap};
 use serde_json::Value;
-use tidal::{encode_base64, get_access_token};
-use std::{error::Error, collections::HashMap};
+use tidal::get_access_token;
+use std::error::Error;
+use std::fs::File;
+use urlencoding;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let client_id = String::from("9Qxzt81m6jPc8yiU");
-    let client_secret = String::from("ACK3uEB9RkDj5NLWzbjND9JesAiGnUaW9SsGw94KxJ8=");
-    let encoded_string = encode_base64(&client_id, &client_secret);
+    let mut input = String::new();
+    println!("Search: ");
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
 
-    let encoded_arg = format!("Basic {}", encoded_string);
+    let type_param = std::borrow::Cow::Borrowed("ALBUMS");
+    let offset_param = std::borrow::Cow::Borrowed("0");
+    let limit_param = std::borrow::Cow::Borrowed("10");
+    let country_code_param = std::borrow::Cow::Borrowed("PL");
+    let popularity_param = std::borrow::Cow::Borrowed("WORLDWIDE");
 
-    let params = [("grant_type", "client_credentials")];
+    let encoded_input = urlencoding::encode(&input.trim());
+    let url = Url::parse_with_params("https://openapi.tidal.com/search?",
+                                                                &[("query", encoded_input),
+                                                                        ("type", type_param),
+                                                                        ("offset", offset_param),
+                                                                        ("limit", limit_param),
+                                                                        ("countryCode", country_code_param),
+                                                                        ("popularity", popularity_param)])?;
 
+    let client_id = String::from("gcjYbogNbf4qm7LQ");
+    let client_secret = String::from("lHANoOjCStwsONMIeGyiT0aef0BmeEemfmIQq7BNqH8=");
     let client = reqwest::Client::new();
 
-    let res = client
-        .post("https://auth.tidal.com/v1/oauth2/token")
-        .header("Authorization", &encoded_arg)
-        .form(&params)
-        .send()
-        .await?
-        .text()
-        .await?;
-
-    let data: Value = serde_json::from_str(&res)?;
-
-    let access_token = data["access_token"].as_str().unwrap();
+    let access_token = get_access_token(&client, &client_id, &client_secret).await?;
     let bearer_token = format!("Bearer {}", &access_token);
-
-    println!("{}", bearer_token);
 
 
     let mut headers = HeaderMap::new();
@@ -38,7 +43,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     headers.insert("Content-Type", HeaderValue::from_static("application/vnd.tidal.v1+json"));
 
     let res = client
-        .get("https://openapi.tidal.com/albums/59727856?countryCode=US")
+        .get(url)
         .headers(headers)
         .send()
         .await?
@@ -47,8 +52,36 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let res_json: Value = serde_json::from_str(&res)?;
 
-    println!("{:?}", res_json);
+    //serde_json::to_writer(&File::create("data.json")?, &res_json)?;
 
+    for album in res_json.as_object().unwrap()["albums"].as_array().unwrap() {
+        let status = album["status"].as_i64().unwrap();
+
+        if status == 200 {
+            let resource = album.as_object().unwrap()["resource"].as_object().unwrap();
+            let artists = resource["artists"].as_array().unwrap();
+            let artists_len = artists.len();
+            let mut artists_name = String::new();
+            let mut counter = 0;
+
+            for artist in artists {
+                artists_name.push_str(artist["name"].as_str().unwrap());
+                if counter != 0 && counter != artists_len {
+                    artists_name.push_str(", ");
+                }
+
+                counter += 1;
+            }
+
+            let album_name = resource["title"].as_str().unwrap();
+            let release_date = resource["releaseDate"].as_str().unwrap();
+            let content = format!("{} - {} {}", artists_name, album_name, release_date);
+
+            println!("{}\n", content);
+        } else {
+            println!("Error {}: {}\n", status, album["message"].as_str().unwrap());
+        }
+    }
 
     Ok(())
 }
